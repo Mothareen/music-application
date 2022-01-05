b5da7c22939f9c80087263195454cac0f2c611ec
Mothareen <m.rahman@stud.fh-sm.de>
2022-01-05 17:59:05 +0100

    update

--- src/interface/imusic.ts
+++ src/interface/imusic.ts
@@ -4,7 +4,7 @@ import { environment } from "src/environments/environment";
 
 export const MusicAppApiPath = {
     // Auth
-    // login: `${environment.nextApiUrl}/method/roona.api.auth.app.login`,
-    TopArtists: `${environment.nextApiUrl}/2.0/?method=tag.gettopartists&tag=disco&api_key=${environment.Api_key}&format=json`,
+    
+    TopArtists: `${environment.MusictApiUrl}/2.0/?method=tag.gettopartists&tag=disco&api_key=${environment.Api_key}&format=json`,
    
   };
